8747d7079a9f358c9955786b603b4ce1fd30db05
jar545 <jl.harm@yahoo.com>
2018-03-15 20:26:02 -0400

    Rough Pass of Analysis Class
    Some classes not even wrote yet

--- Texas Holdem/Poker/src/poker/Analysis.java
+++ Texas Holdem/Poker/src/poker/Analysis.java
@@ -1,129 +1,157 @@
 package poker;
-//to do
 
-//Make Straight and flush look for highest straight and flush to look at the hightest flush
-// fix straight flush to look at 
-import java.util.Arrays;
-import java.util.Collections;
+
 import poker.Objects.Card;
 
-/**
- *
- * @author Jared
- */
+
 public class Analysis {
 
     /**
      * Hand Types 0 = High Card 1 = Pair 2 = Two Pair 3 = Three of a kind 4 =
      * Straight 5 = Flush 6 = Full House 7 = Four of a kind 8 = Straight Flush
      */
-    //set the defualt hand type to high card
+    
     private int handType = 0;
+    @SuppressWarnings("FieldMayBeFinal")
     private Card[] hand;
 
     //Constructor
     public Analysis(Card[] hand) {
         this.hand = hand;
         System.out.println("Analysis Object Created");
-        
+
     }
 
     public void getHandType() {
-        //Sort Cards in hand by rank
+               
+        printHand();
+        System.out.println(isFlush());
+        printHand();
         
-        System.out.println("System Check");
-        sortByRank();
-        System.out.println("System Check ");
-        System.out.println("Hand Sorted By Rank ");
-        System.out.println("Hand: ex - C(Rank,Suit)");
-        for (int i = 0; i < hand.length; i++) {
-            System.out.println(" C(" + hand[i].getRank() + ", " + hand[i].getSuit());
-        }
-        
-        
-
-        
-        
-        //Start looking for highest ranked hand first
-        //if (straightFlush()) {
-            //handType = 8;
-        //} else if (fourOfAKind()) {
-          //  handType = 7;
-        //} else if (fullHouse()) {
-          //  handType = 6;
-        //} else if (flush()) {
-            //handType = 5;
-        //} else if (straight()) {
-            //handType = 4;
-        //} else if (threeOfAKind()) {
-          //  handType = 3;
-        //} else if (twoPair()) {
-            //handType = 2;
-        //} else if (pair()) {
-          //  handType = 1;
-        //} else if (highCard()) {
-          //  handType = 0;
-        //}
     }
-
-   
-
+        
+    //Sorts a hand of cards by rank in incresing order
     private void sortByRank(Card[] hand) {
-        
-    
-   }
-        
-    }
-    
-    private void sortBySuit(){
-        Card tempCard;
-        for (int i = 1; i < hand.length; i++) {
-            while (hand[i].getSuit() < hand[i--].getSuit()) {
-                swap(i,i--);
+
+        for (int j = 0; j < hand.length - 1; j++) {
+            int iMin = j;
+
+            for (int i = j + 1; i < hand.length; i++) {
+                if (hand[i].getRank() < hand[iMin].getRank()) {
+                    iMin = i;
+                }
+                
             }
             
-        }
-    }
-    
-    private void swap(int i, int x){
-        Card tempCard = hand[i--];
-        hand[i--] = hand[i];
-        hand[i] = tempCard;
-    }
-
-    private boolean straight(){
-        boolean isStraight = true;
-        
-        for (int i = 0; i < 5; i++) {
-            if (hand[i].getRank() == (hand[i++].getRank()-1)) {
-                isStraight = false;
+            if (iMin != j) {
+                swap(hand,j,iMin);
             }
         }
-        
-        return isStraight;
-    }
-    
-    /*
-    private boolean flush(){
-        boolean isFlush = false;
-        
-        if (hand[i].getSuit() == hand[4].getRank()) {
-            isFlush = true;
-        }
-        
-        return isFlush;
-    }
-    */
-    
-    /*
-    private boolean straightFlush(){
-        boolean isStraightFlush = false;
-        if (straight() && flush()) {
-            isStraightFlush = true;
-        }
-        return isStraightFlush;
-    }
-    */
 
+    }
     
+    //swaps 2 cards of given index in a given hand
+    private void swap(Card[] hand, int s1, int s2) {
+        Card t;
+        t = hand[s1];
+        hand[s1] = hand[s2];
+        hand[s2] = t;
+
+    }
+
+    //sorts a hand of card objects by suit| Sorted by arbitrary number assigned to rank
+    private void sortBySuit() {
+        for (int j = 0; j < hand.length - 1; j++) {
+            int iMin = j;
+
+            for (int i = j + 1; i < hand.length; i++) {
+                if (hand[i].getSuit() < hand[iMin].getSuit()) {
+                    iMin = i;
+                }
+                
+            }
+            
+            if (iMin != j) {
+                swap(hand,j,iMin);
+            }
+        }
+    }
+    
+    //prints the hand to the console
+    private void printHand(){
+        for (Card hand1 : hand) {
+            System.out.println(" C(" + hand1.getRank() + ", " + hand1.getSuit() + ")");
+        }
+    }
+    
+    
+    private boolean isStraightFlush() {
+        if (isStraight() && isFlush()) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isFourOfAKind() {
+        if (
+                hand[0].getSuit() ==
+                hand[1].getSuit() && 
+                hand[1].getSuit() == 
+                hand[2].getSuit() &&
+                hand[2].getSuit() == 
+                hand[3].getSuit()  
+                
+            ){
+            return true;
+        }
+        else if(
+                hand[1].getSuit() ==
+                hand[2].getSuit() && 
+                hand[2].getSuit() == 
+                hand[3].getSuit() &&
+                hand[3].getSuit() == 
+                hand[4].getSuit()   
+
+            ){
+            
+        }
+        return false;
+    }
+
+    private boolean isFullHouse() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private boolean isFlush() {
+        sortBySuit();
+        
+        if (hand[0].getSuit() == hand[hand.length-1].getSuit()) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isStraight() {
+        sortByRank(hand);
+        int testRank = hand[0].getRank();
+        for (int i = 0; i < hand.length; i++) {
+            if (hand[i].getRank() != testRank) {
+                return false;
+            }
+            testRank++;
+        }
+        return true;
+    }
+
+    private boolean isThreeOfAKind() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private boolean isTwoPair() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private boolean isPair() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
